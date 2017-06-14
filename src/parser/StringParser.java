2936b032f4271d03be92f91b8bfd108161098c86
jhon50 <johnn_lds@hotmail.com>
2017-06-14 12:38:23 -0300

    adiciona classe StringParser

---
+++ src/parser/StringParser.java
@@ -0,0 +1,26 @@
+/**
+ * Created by desenvolvedor on 14/06/17.
+ */
+public class StringParser {
+
+    public static String[] splitByNumber(String text, int number) {
+        int inLength = text.length();
+        int arLength = inLength / number;
+        int left = inLength % number;
+        if(left>0){++arLength;}
+        String ar[] = new String[arLength];
+        String tempText=text;
+        for (int x = 0; x < arLength; ++x) {
+
+            if(tempText.length()>number){
+                ar[x]=tempText.substring(0, number);
+                tempText=tempText.substring(number);
+            }else{
+                ar[x]=tempText;
+            }
+
+        }
+
+        return ar;
+    }
+}
