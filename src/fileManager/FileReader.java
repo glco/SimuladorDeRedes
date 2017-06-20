815839963eeb6c515febcaf7eb32ba985081db8d
jhon50 <johnn_lds@hotmail.com>
2017-06-20 16:19:48 -0300

    fileReader

--- src/fileManager/FileReader.java
+++ src/fileManager/FileReader.java
@@ -1,24 +1,20 @@
 package fileManager;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.LinkedList;
-import java.io.IOException;
 
-public class fileReader {
+public class FileReader {
 
-    public LinkedList<String[]> readNetworkFile(String fileName) {
-        LinkedList<String[]> array = new LinkedList<>();
-        BufferedReader buffer = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+    public LinkedList<String[]> readNetworkFile(String fileName) throws IOException {
         String s = null;
         String [] aux = null;
+        LinkedList<String[]> array = new LinkedList<>();
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
         while((s = buffer.readLine()) != null){
             aux = s.split(" ");
             array.push(aux);
         }
         buffer.close();
-
         return array;
     }
 }
