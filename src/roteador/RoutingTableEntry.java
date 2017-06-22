49d24622394ecd3a4c643c285460a2d24a537104
jhon50 <johnn_lds@hotmail.com>
2017-06-22 16:35:08 -0300

    inicializa variáveis

--- src/roteador/RoutingTableEntry.java
+++ src/roteador/RoutingTableEntry.java
@@ -6,9 +6,9 @@ import java.util.LinkedList;
  * Created by desenvolvedor on 22/06/17.
  */
 public class RoutingTableEntry {
-    private String SubnetAddress;
-    private String SubnetMask;
-    private int interfaceIndex;
+    private String SubnetAddress = null;
+    private String SubnetMask = null;
+    private int interfaceIndex = 0;
 
     public RoutingTableEntry(String[] row){
         if(row.length < 3) return;
